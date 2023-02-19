55ec722b16aea8003a9f98e5f54bef76ba7b766f
SurajGoel <sgoel01@gmail.com>
2023-02-19 13:33:06 +0530

    changes

--- src/buffer/buffer_pool_manager_instance.cpp
+++ src/buffer/buffer_pool_manager_instance.cpp
@@ -102,7 +102,7 @@ auto BufferPoolManagerInstance::FetchPgImp(page_id_t page_id) -> Page * {
   if (page_table_->Find(page_id, frame_id)) {
     Page *existing_page = &pages_[frame_id];
     existing_page->WLatch();
-    existing_page->pin_count_ = existing_page->GetPinCount() + 1;
+    existing_page->pin_count_ = existing_page->pin_count_++;
     PostSuccessfullPageAllocation(page_id, frame_id);
     existing_page->WUnlatch();
     return existing_page;
@@ -117,6 +117,8 @@ auto BufferPoolManagerInstance::FetchPgImp(page_id_t page_id) -> Page * {
     latch_.unlock();
 
     Page *existing_page = &pages_[frame_id];
+    page_table_->Remove(existing_page->GetPageId());
+
     existing_page->WLatch();
     ResetPageMetadata(*existing_page);
     existing_page->ResetMemory();
@@ -165,8 +167,8 @@ auto BufferPoolManagerInstance::UnpinPgImp(page_id_t page_id, bool is_dirty) -> 
 
   Page *existing_page = &pages_[frame_id];
   existing_page->WLatch();
-  if (existing_page->GetPinCount() <= 0) {
-    replacer_->SetEvictable(frame_id, true);
+  if (existing_page->pin_count_ <= 0) {
+//    replacer_->SetEvictable(frame_id, true);
     existing_page->WUnlatch();
     return false;
   }
@@ -175,14 +177,11 @@ auto BufferPoolManagerInstance::UnpinPgImp(page_id_t page_id, bool is_dirty) -> 
     existing_page->is_dirty_ = is_dirty;
   }
 
+  existing_page->pin_count_ = existing_page->GetPinCount() - 1;
   if (existing_page->GetPinCount() == 0) {
     replacer_->SetEvictable(frame_id, true);
-  } else {
-    existing_page->pin_count_ = existing_page->GetPinCount() - 1;
-    if (existing_page->GetPinCount() == 0) {
-      replacer_->SetEvictable(frame_id, true);
-    }
   }
+
   existing_page->WUnlatch();
 
   return true;
